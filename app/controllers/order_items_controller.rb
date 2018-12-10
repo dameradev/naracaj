2abdc4646323c9851fb7703a57d6dc613b438346
=DamjanRadev <radevdamjan@gmail.com>
2018-12-10 15:09:10 +0100

    Modified order path to stay on firms page

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,14 +1,17 @@
 class OrderItemsController < ApplicationController
   def index
   @items = current_cart.order.items
+
 end
  def create
+   @product = Product.find(params[:product_id])
+   @firm = @product.firm
    unless current_user.is_a?(GuestUser)
     current_cart.add_item(
       product_id: params[:product_id],
       quantity: params[:quantity]
     )
-     redirect_to cart_path
+     redirect_to firm_products_url(@firm)
    else
      redirect_to new_user_session_path
      flash[:notice] = "You must be logged in!"
