2359ac57daf68179b49d639c19536b8f25750e1c
=DamjanRadev <radevdamjan@gmail.com>
2019-04-08 20:05:19 +0200

    Fixed model address error, fixed yarn warrning

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders
   has_many :addresses
-  validates :addresses, :length => { :minimum => 1 }
-
   
+
+
 end
